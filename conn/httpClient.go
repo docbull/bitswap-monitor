2833caf7dda2a7144584593c8babc3f42db98152
docbull <fudghks21@gmail.com>
2022-07-03 16:13:51 +0900

    http client for connecting to ipfs node

---
+++ conn/httpClient.go
@@ -0,0 +1,28 @@
+package conn
+
+import (
+	"net/http"
+	"os"
+	"time"
+)
+
+type HttpClient struct {
+	Client *http.Client
+	URL    string
+}
+
+func NewHTTPClient() *HttpClient {
+	url := "http://localhost:5001"
+	if len(os.Args) > 1 {
+		url = os.Args[1]
+	}
+	return &HttpClient{
+		Client: &http.Client{
+			Transport:     nil,
+			CheckRedirect: nil,
+			Jar:           nil,
+			Timeout:       time.Second * 10,
+		},
+		URL: url + "/api/v0/",
+	}
+}
