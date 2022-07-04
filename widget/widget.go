d125a7a2250dbb5b83bc88c9ab95c475a68f7cca
Ryeong Hwan Kim <krh@Ryeongs-MacBook-Pro.local>
2022-07-04 22:26:05 +0900

    widget for UI

---
+++ widget/widget.go
@@ -0,0 +1,49 @@
+package widget
+
+import (
+	"github.com/docbull/bitswap-monitor/conn"
+	"github.com/rivo/tview"
+)
+
+type Widget struct {
+	View   *tview.TextView
+	Client *conn.Client
+}
+
+func NewWidget() Widget {
+	view := tview.NewTextView()
+	view.SetBackgroundColor()
+	view.SetBorder(true)
+	view.SetBorderColor()
+	view.SetDynamicColors(true)
+	view.SetTextColor()
+	view.SetTitleColor()
+	view.SetWrap(false)
+	view.SetScrollable(true)
+
+	return Widget{
+		View: view,
+		// Client:
+	}
+}
+
+func NewTviewGrid() *tview.Grid {
+	grid := tview.NewGrid()
+	grid.SetBackgroundColor()
+	grid.SetColumns()
+	grid.SetRows()
+	grid.SetBorder(false)
+
+	return grid
+}
+
+func NewTviewApp(grid *tview.Grid) *tview.Application {
+	pages := tview.NewPages()
+	pages.AddPage("grid", grid, true, true)
+	pages.Box.SetBackgroundColor()
+
+	tviewApp := tview.NewApplication()
+	tviewApp.SetRoot(pages, true)
+
+	return tviewApp
+}
