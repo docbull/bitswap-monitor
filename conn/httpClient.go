35715a0e446b241d565ad1fb3c8c1ce8f0582900
docbull <fudghks21@gmail.com>
2022-07-15 22:46:17 +0900

    HTTP client for retrieving bitswap stat from ipfs node

--- conn/httpClient.go
+++ conn/httpClient.go
@@ -11,24 +11,6 @@ type HttpClient struct {
 	URL    string
 }
 
-// func NewHTTPClient() fx.Option {
-// 	return fx.Provide(func() *HttpClient {
-// 		url := "http://localhost:5001"
-// 		if len(os.Args) > 1 {
-// 			url = os.Args[1]
-// 		}
-// 		return &HttpClient{
-// 			Client: &http.Client{
-// 				Transport:     nil,
-// 				CheckRedirect: nil,
-// 				Jar:           nil,
-// 				Timeout:       time.Second * 10,
-// 			},
-// 			URL: url + "/api/v0/",
-// 		}
-// 	})
-// }
-
 func NewHTTPClient() *HttpClient {
 	url := "http://localhost:5001"
 	if len(os.Args) > 1 {
