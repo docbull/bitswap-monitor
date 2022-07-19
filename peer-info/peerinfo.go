f3c3d3dab5064fc484a68272f57ac913c0b4a440
docbull <fudghks21@gmail.com>
2022-07-20 03:46:14 +0900

    ipfs id extractor

--- peer-info/peerinfo.go
+++ peer-info/peerinfo.go
@@ -1,9 +1,41 @@
 package peerinfo
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/docbull/bitswap-monitor/conn"
+	"github.com/rivo/tview"
+)
+
 type PeerInfo struct {
 	Addresses       []string `json:"Addresses"`
 	AgentVersion    string   `json:"AgentVersion"`
 	ID              string   `json:"ID"`
 	ProtocolVersion string   `json:"ProtocolVersion"`
 	PublicKey       string   `json:"PublicKey"`
+}
+
+// ShowPeerInfo prints IPFS node's ID
+func ShowPeerInfo(client *conn.HttpClient, peerInfo *PeerInfo, view *tview.TextView) {
+	text := ""
+	var data = []byte{}
+	req, err := http.NewRequest("POST", client.URL+"id", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	data, _ = ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(data, &peerInfo)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	text += fmt.Sprintf("ID: %v", peerInfo.ID)
+	fmt.Fprintf(view, "%s", text)
 }
