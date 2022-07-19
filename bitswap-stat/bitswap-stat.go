90a0ab6765bf9157e4967f71ee3bd88c513694e3
docbull <fudghks21@gmail.com>
2022-07-20 03:45:41 +0900

    ipfs bitswap stats for logging status

--- bitswap-stat/bitswap-stat.go
+++ bitswap-stat/bitswap-stat.go
@@ -1,5 +1,16 @@
 package bitswap_stat
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+
+	"github.com/docbull/bitswap-monitor/conn"
+	"github.com/rivo/tview"
+)
+
 type BitswapStat struct {
 	BlocksReceived   uint64        `json:"BlocksReceived"`
 	BlocksSent       uint64        `json:"BlocksSent"`
@@ -11,4 +22,36 @@ type BitswapStat struct {
 	Peers            []interface{} `json:"Peers"`
 	ProvideBufLen    int           `json:"ProvideBufLen"`
 	Wantlist         []interface{} `json:"Wantlist"`
+}
+
+// RefreshMonitor re-rendering bitswap logs every 500ms.
+func RefreshMonitor(client *conn.HttpClient, bitswapStat *BitswapStat, view *tview.TextView) {
+	text := ""
+	var data = []byte{}
+
+	req, err := http.NewRequest("POST", client.URL+"bitswap/stat", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	data, _ = ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(data, &bitswapStat)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < len(bitswapStat.Wantlist); i++ {
+		a := reflect.ValueOf(bitswapStat.Wantlist[i])
+		for _, b := range a.MapKeys() {
+			c := a.MapIndex(b)
+			t := c.Interface()
+			text += fmt.Sprintf("%v ", t)
+		}
+	}
+	if text != "" {
+		fmt.Fprintf(view, "%s\n\n", text)
+	}
 }
